3363442ad3fc71033560de42fb9b2f9ed56e6da4
山内健嗣 <yamauchikenshi@yamauchikenjinoMacBook-Air.local>
2026-01-31 04:40:49 +0900

    style about page

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/works")
+def works():
+    return render_template("works.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
